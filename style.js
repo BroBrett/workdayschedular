b9869cc9d370b20c32084a3a38b0cc7052875a65
brett hockridge <bretthockridge98@gmail.com>
2022-04-22 12:45:56 -0700

    added buttons and text area to timeblocks. Added current time and date to javaScript

--- style.js
+++ style.js
@@ -1,6 +1,25 @@
-var timeDisplayEl = $('#time-display');
+let timeDisplay = $('#currentDay');
 
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
+
+function runningTime() {
+    let currentTimeDate = (moment().format('dddd, MMMM Do YYYY h:mm:ss a'));
+    timeDisplay.text(currentTimeDate);
+    setInterval(runningTime, 1000);
+}
+
+
+function renderTimeColor() {
+    const currentHour = moment().hours();
+    $('.time-block').each(function() {
+        const rowHour = parseInt($(this).attr('id'));
+        if (rowHour < currentHour) {
+            $(this).addClass('past');
+        } else if (rowHour === currentHour) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        };
+    });
+};
+
+runningTime();
